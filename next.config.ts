988734e004774c78702bebe4c40395665110b960
Felipe Freitas <canais.ffasilveira@voegol.com.br>
2025-03-25 10:43:50 -0300

    feat: adding rewrites for API

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "https://cylindrical-bird-tikpmfl16khdrud9ijmvg8yd.herokudns.com/api/:path*", // Proxy para o Heroku
+      },
+    ];
+  },
 };
 
 export default nextConfig;
